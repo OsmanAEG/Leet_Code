f58da71e6ce274620087269a25e155480cae80f7
Osman El-Ghotmi <79581083+OsmanAEG@users.noreply.github.com>
2025-06-30 23:47:04 -0400

    find the original typed string i

---
+++ find_the_original_typed_string_i.cpp
@@ -0,0 +1,29 @@
+#include <string>
+
+class Solution {
+public:
+  int possibleStringCount(std::string word) {
+    int mult = 1;
+
+    int idx = 0;
+
+    auto c = word[idx];
+
+    while(idx < word.size() - 1) {
+      int num_it = 0;
+
+      while(c == word[idx + 1]) {
+        ++idx;
+        ++num_it;
+      }
+
+      std::cout << num_it << std::endl;
+
+      ++idx;
+      mult += num_it;
+      c = word[idx];
+    }
+
+    return mult;
+  }
+};
