7249328f71cf8243e62a2b9d1a4089405cc2632f
Osman El-Ghotmi <79581083+OsmanAEG@users.noreply.github.com>
2025-09-28 17:28:08 -0400

    largest perimeter triangle

--- largest_perimeter_triangle.cpp
+++ largest_perimeter_triangle.cpp
@@ -5,26 +5,13 @@
 
 class Solution {
 public:
-  bool is_valid_triangle(int a, int b, int c) {
-    if(b + c > a) return true;
-    else return false;
-  }
-
-  int calculate_perimeter(int a, int b, int c) {
-    return a + b + c;
-  }
-
   int largestPerimeter(std::vector<int>& nums) {
-    int max = 0;
     std::sort(nums.rbegin(), nums.rend());
 
     for(int i = 0; i < nums.size() - 2; ++i) {
-      if(is_valid_triangle(nums[i], nums[i+1], nums[i+2])) {
-        max = std::max(max, calculate_perimeter(nums[i], nums[i+1], nums[i+2]));
-        break;
-      }
+      if(nums[i+1] + nums[i+2] > nums[i]) return nums[i] + nums[i+1] + nums[i+2];
     }
 
-    return max;
+    return 0;
   }
 };
