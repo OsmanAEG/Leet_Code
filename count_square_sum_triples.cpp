070d40ecd92d4c42831bd5be5e7042d6271687c8
Osman El-Ghotmi <79581083+OsmanAEG@users.noreply.github.com>
2026-01-11 07:21:16 -0500

    count square sum triplets

---
+++ count_square_sum_triples.cpp
@@ -0,0 +1,27 @@
+// LeetCode: 1925
+
+#include <unordered_map>
+
+class Solution {
+public:
+  bool is_square_triple(int a, int b, int c) {
+    if(a*a + b*b == c*c) return true;
+    else return false;
+  }
+
+  int countTriples(int n) {
+    std::map<int, int> ab;
+    
+    int num = 0;
+
+    for(int i = 1; i <= n; ++ i) {
+      for(int j = 1; j <= n; ++j) {
+        for(int k = 1; k <= n; ++k) {
+          if(is_square_triple(i, j, k)) ++num;
+        }
+      }
+    }
+
+    return num;
+  }
+};
