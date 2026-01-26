1d2a0513d91418dd134c3ebca0d646026e34ad48
Osman El-Ghotmi <79581083+OsmanAEG@users.noreply.github.com>
2026-01-26 06:39:42 -0500

    minimum absolute difference

---
+++ minimum_absolute_difference.cpp
@@ -0,0 +1,26 @@
+// LeetCode: 1200
+
+#include <algorithm>
+#include <climits>
+#include <vector>
+
+class Solution {
+public:
+  std::vector<std::vector<int>> minimumAbsDifference(std::vector<int>& arr) {
+    std::vector<std::vector<int>> pairs;
+
+    int min_diff = INT_MAX;
+
+    std::sort(arr.begin(), arr.end());
+
+    for(int i = 0; i < arr.size() - 1; ++i) {
+      min_diff = std::min(arr[i+1] - arr[i], min_diff);
+    }
+
+    for(int i = 0; i < arr.size() - 1; ++i) {
+      if(arr[i+1] - arr[i] == min_diff) {
+        pairs.push_back({arr[i], arr[i+1]});
+      }
+    }
+  }
+};
