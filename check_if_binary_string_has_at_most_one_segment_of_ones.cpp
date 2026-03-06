780c138e5500f0fcddf49fc1685166aca3412112
Osman El-Ghotmi <79581083+OsmanAEG@users.noreply.github.com>
2026-03-06 09:16:00 -0500

    check if binary string has at most one segment of ones

---
+++ check_if_binary_string_has_at_most_one_segment_of_ones.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+class Solution {
+public:
+  bool checkOnesSegment(std::string s) {
+    bool counted_0 = false;
+
+    for(const auto& c : s) {
+      if(c == '0') counted_0 = true;
+      if(c == '1' && counted_0 == true) return false;
+    }
+
+    return true;
+  }
+};
---
+++ find_minimum_in_rotated_sorted_array.cpp
@@ -0,0 +1,15 @@
+// LeetCode: 153
+
+#include <vector>
+
+class Solution {
+public:
+  int findMin(std::vector<int>& nums) {
+    int low  = 0;
+    int high = nums.size() - 1;
+
+    while(low < high) {
+      int mid = (low + high)/2;
+    }
+  }
+};
